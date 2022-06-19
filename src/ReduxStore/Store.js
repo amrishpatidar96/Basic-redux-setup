3ac00cdd1179d5e4e164c85d8e1390d3488fbf11
amrishpatidar96 <amrishap90@gmail.com>
2022-06-19 23:47:18 +0530

    basic redux setup

---
+++ src/ReduxStore/Store.js
@@ -0,0 +1,20 @@
+import { applyMiddleware ,createStore } from 'redux'
+
+import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+//import monitorReducersEnhancer from './enhancers/monitorReducers'
+//import loggerMiddleware from './middleware/logger'
+import rootReducer from './Reducers'
+
+export default function configureStore(preloadedState) {
+  const middlewares = [thunkMiddleware]
+  const middlewareEnhancer = applyMiddleware(...middlewares)
+
+  const enhancers = [middlewareEnhancer]
+  const composedEnhancers = composeWithDevTools(...enhancers)
+
+  const store = createStore(rootReducer, preloadedState, composedEnhancers)
+
+  return store
+}
