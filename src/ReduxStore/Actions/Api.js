2f85099253a7bb7f39ca1e196c35ccea41c9b7a3
Amrish Patidar <amrishap90@gmail.com>
2022-06-20 15:49:46 +0530

    boiler plate

---
+++ src/ReduxStore/Actions/Api.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+import {BASE_URL} from "./ApiEndpoints";
+const axiosInstance = axios.create({ baseURL: BASE_URL });
+
+function get(endpoint, data = {}, responseType) {
+  return axiosInstance
+    .get(endpoint, { data, responseType });
+}
+
+// function post(endpoint, data = {}, headers) {
+//   return axiosInstance
+//     .post(endpoint, data, { headers })
+//     .then((res) => {
+//       return res;
+//     })
+//     .catch((error) => {
+//       return error;
+//     });
+// }
+
+
+// function put(endpoint, data) {
+//   return axiosInstance
+//     .put(endpoint, data)
+//     .then((res) => {
+//       return res;
+//     })
+//     .catch((error) => {
+//       return error;
+//     });
+// }
+
+// function _delete(endpoint, data) {
+//   return axiosInstance
+//     .delete(endpoint, data)
+//     .then((res) => {
+//       return res;
+//     })
+//     .catch((error) => {
+//       return error;
+//     });
+// }
+
+// function patch(endpoint, data) {
+//   return axiosInstance
+//     .patch(endpoint, data)
+//     .then((res) => {
+//       return res;
+//     })
+//     .catch((error) => {
+//       return error;
+//     });
+// }
+
+export default {
+  get,
+  axiosInstance
+};
